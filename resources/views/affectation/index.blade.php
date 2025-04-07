98045e125cc0c25b6c8eda9b8de4d1202c4453b0
Ryky16 <r.rykydiatta@gmail.com>
2025-04-07 11:56:16 +0000

    comit

--- resources/views/affectation/index.blade.php
+++ resources/views/affectation/index.blade.php
@@ -1,12 +1,26 @@
 <x-app-layout>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
-            <h2 class="text-2xl font-bold text-center text-green-700 mb-4">📌 Affecter un Courrier</h2>
-            <p class="text-sm text-center text-gray-600 mb-6">Remplissez les informations pour affecter un courrier</p>
+    <div class="flex items-center justify-center min-h-screen bg-gray-200">
+        <div class="w-full max-w-6xl bg-white rounded-lg shadow-lg p-8">
+            <h2 class="text-2xl font-bold text-center text-green-700 mb-6">📌 Ajouter un Courrier</h2>
+            <p class="text-sm text-center text-gray-600 mb-6">Remplissez les informations pour ajouter un courrier</p>
 
             {{-- Formulaire pour affecter un courrier --}}
             <form action="{{ route('courriers.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
+
+                    <!-- Type -->
+                    <div class="mb-4">
+                    <label for="type" class="block text-gray-700 font-bold flex items-center">
+                        <span class="mr-2">🔄</span> Type
+                    </label>
+                    <div class="relative">
+                        <select id="type" name="type" 
+                            class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-500 focus:border-green-500">
+                            <option value="entrant">Entrant</option>
+                            <option value="sortant">Sortant</option>
+                        </select>
+                    </div>
+                </div>
 
                 <!-- Référence -->
                 <div class="mb-4">
@@ -118,11 +132,13 @@
                 </div>
 
                 <!-- Bouton d'envoi -->
-                <button type="submit" 
-                    class="w-full bg-black py-3 rounded-lg font-semibold text-lg hover:bg-gray-800 hover:scale-105 transition-transform duration-300">
-                    📩 Envoyer le courrier
-                </button>
+            <button type="submit" 
+                 class="w-full bg-black text-white py-3 rounded-lg font-semibold text-lg border-2 border-gray-700 hover:bg-gray-800 hover:border-gray-600 hover:scale-105 transition-transform duration-300">
+                  📩 Envoyer le courrier
+            </button>
+            <script src="https://cdn.tailwindcss.com"></script>
             </form>
         </div>
     </div>
+   
 </x-app-layout>
