54b0ffd8a0ac93f2fae302e5c99ee1843478d016
Ryky16 <r.rykydiatta@gmail.com>
2025-04-07 15:32:16 +0000

    comit

--- resources/views/affectation/index.blade.php
+++ resources/views/affectation/index.blade.php
@@ -4,9 +4,23 @@
             <h2 class="text-2xl font-bold text-center text-green-700 mb-6">📌 Ajouter un Courrier</h2>
             <p class="text-sm text-center text-gray-600 mb-6">Remplissez les informations pour ajouter un courrier</p>
 
-            {{-- Formulaire pour affecter un courrier --}}
+
+            {{-- Formulaire pour ajouter un courrier --}}
+
             <form action="{{ route('courriers.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
+
+
+                 <!-- date du courrier-->
+                 <div class="mb-4">
+    <label for="date_courrier" class="block text-gray-700 font-bold flex items-center">
+        <span class="mr-2">📅</span> Date du courrier
+    </label>
+    <div class="relative">
+        <input type="date" id="date_courrier" name="date_courrier"
+            class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-500 focus:border-green-500">
+    </div>
+</div>
 
                     <!-- Type -->
                     <div class="mb-4">
@@ -78,20 +92,6 @@
                     </div>
                 </div>
 
-                <!-- Type -->
-                <div class="mb-4">
-                    <label for="type" class="block text-gray-700 font-bold flex items-center">
-                        <span class="mr-2">🔄</span> Type
-                    </label>
-                    <div class="relative">
-                        <select id="type" name="type" 
-                            class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-500 focus:border-green-500">
-                            <option value="entrant">Entrant</option>
-                            <option value="sortant">Sortant</option>
-                        </select>
-                    </div>
-                </div>
-
                 <!-- Contenu -->
                 <div class="mb-4">
                     <label for="description" class="block text-gray-700 font-bold flex items-center">
@@ -125,12 +125,13 @@
                         <select id="priorite" name="priorite" 
                             class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-500 focus:border-green-500">
                             <option value="basse">Basse</option>
-                            <option value="normale" selected>Normale</option>
-                            <option value="élevée">Élevée</option>
+                            <option value="moyenne" selected>Moyenne</option>
+                            <option value="haute">Haute</option>
                         </select>
                     </div>
                 </div>
 
+               
                 <!-- Bouton d'envoi -->
             <button type="submit" 
                  class="w-full bg-black text-white py-3 rounded-lg font-semibold text-lg border-2 border-gray-700 hover:bg-gray-800 hover:border-gray-600 hover:scale-105 transition-transform duration-300">
